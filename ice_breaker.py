5fda6786444b363c984f3941bbd124b4093a4ec2
Timo van Niedek <timovanniedek@gmail.com>
2024-05-18 15:50:48 +0200

    ✨ Connect linkedin agent with summary template

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,9 +1,13 @@
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains.llm import LLMChain
+from icebreaker.agents import linkedin_lookup_agent
 from icebreaker.scraping.linkedin import scrape_linkedin_profile
 
-if __name__ == "__main__":
+def ice_break_with(name: str, mock=False) -> str:
+    linkedin_url = linkedin_lookup_agent.lookup(name)
+    linkedin_data = scrape_linkedin_profile(linkedin_url, mock=mock)
+
     summary_template = """
     Given the information {information} about a person, I want you to create:
     1. A short summary
@@ -15,11 +19,11 @@ if __name__ == "__main__":
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://linkedin.com/in/timo-van-niedek/", mock=True
-    )
+    chain = summary_prompt_template | llm
     res = chain.invoke(input={"information": linkedin_data})
+    print(res.content)
 
-    print(res["text"])
+
+if __name__ == "__main__":
+    print("Ice breaking with Timo van Niedek")
+    ice_break_with("Timo van Niedek", mock=True)
