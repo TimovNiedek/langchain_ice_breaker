ac27c31ba490f93ff08ee93647875ca1eb7bc0e5
Timo van Niedek <timovanniedek@gmail.com>
2024-05-20 11:57:15 +0200

    💄 Add a simple UI and style.css

---
+++ icebreaker/app.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
