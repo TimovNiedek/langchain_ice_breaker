6c15e9595015a64b81e1694f9a7c47e921430fdf
Timo van Niedek <timovanniedek@gmail.com>
2024-05-20 11:33:46 +0200

    🎨 Return summary object and profile pic url

--- icebreaker/main.py
+++ icebreaker/main.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional, Tuple
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from icebreaker.agents import linkedin_lookup_agent
@@ -6,7 +7,7 @@ from icebreaker.scraping.linkedin import scrape_linkedin_profile
 from icebreaker.output_parsers import Summary, summary_parser
 
 
-def ice_break_with(name: str, mock=False) -> str:
+def ice_break_with(name: str, mock=False) -> Tuple[Summary, Optional[str]]:
     linkedin_url = linkedin_lookup_agent.lookup(name, mock=mock)
     linkedin_data = scrape_linkedin_profile(linkedin_url, mock=mock)
 
@@ -29,9 +30,10 @@ def ice_break_with(name: str, mock=False) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = summary_prompt_template | llm | summary_parser
     summary: Summary = chain.invoke(input={"information": linkedin_data})
-    print(json.dumps(summary.dict(), indent=4))
+    return summary, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
-    print("Ice breaking with Timo van Niedek")
-    ice_break_with("Timo van Niedek", mock=True)
+    summary, profile_pic_url = ice_break_with("Eden Marco Udemy", mock=False)
+    print(json.dumps(summary.dict(), indent=4))
+    print(profile_pic_url)
